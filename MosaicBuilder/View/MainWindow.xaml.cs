aa9623d49160405c5bfcffac739263c5f3d0da60
Tyler Gosling <t.gosling61@gmail.com>
2019-05-19 12:39:31 -0400

    Ellipse prep for pallete view

--- MosaicBuilder/View/MainWindow.xaml.cs
+++ MosaicBuilder/View/MainWindow.xaml.cs
@@ -1,17 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MosaicBuilder.ViewModel;
+
 
 namespace MosaicBuilder
 {
@@ -20,9 +15,33 @@ namespace MosaicBuilder
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Attributes for Mosaic Pallette
+        private Point startPoint;
+        private List<Ellipse> source = null;
+
+        //Mosaic Pallette
+        Ellipse e1 = new Ellipse() { Width = 15, Height = 15, Fill = Brushes.Black };
+        Ellipse e2 = new Ellipse() { Width = 15, Height = 15, Fill = Brushes.Red };
+        Ellipse e3 = new Ellipse() { Width = 15, Height = 15, Fill = Brushes.Crimson };
+        Ellipse e4 = new Ellipse() { Width = 15, Height = 15, Fill = Brushes.IndianRed };
+        Ellipse e5 = new Ellipse() { Width = 15, Height = 15, Fill = Brushes.Orange };
+        Ellipse e6 = new Ellipse() { Width = 15, Height = 15, Fill = Brushes.DarkOrange };
+        Ellipse e7 = new Ellipse() { Width = 15, Height = 15, Fill = Brushes.Yellow };
+        Ellipse e8 = new Ellipse() { Width = 15, Height = 15, Fill = Brushes.GreenYellow };
+        Ellipse e9 = new Ellipse() { Width = 15, Height = 15, Fill = Brushes.Green };
+        Ellipse e10 = new Ellipse() { Width = 15, Height = 15, Fill = Brushes.Blue };
+        Ellipse e11 = new Ellipse() { Width = 15, Height = 15, Fill = Brushes.Indigo };
+        Ellipse e12 = new Ellipse() { Width = 15, Height = 15, Fill = Brushes.Violet };
+        Ellipse e13 = new Ellipse() { Width = 15, Height = 15, Fill = Brushes.Black };
+        Ellipse e14 = new Ellipse() { Width = 15, Height = 15, Fill = Brushes.White };
+        Ellipse e15 = new Ellipse() { Width = 15, Height = 15, Fill = Brushes.Gray };
+
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new ViewModelMain();
+            Ellipse[] palletteArray = { e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15 };
+            source = new List<Ellipse>(palletteArray);
         }
     }
 }
