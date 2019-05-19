c3bb026a104cd4d521cbdcbb6ddcadfed5681b8e
Tyler Gosling <t.gosling61@gmail.com>
2019-05-19 12:18:02 -0400

    View Model basis

---
+++ MosaicBuilder/ViewModel/ViewModelMain.cs
@@ -0,0 +1,25 @@
+﻿
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace MosaicBuilder.ViewModel
+{
+    class ViewModelMain : INotifyPropertyChanged
+    {
+       
+        //Handle File->Exit menu click
+        private void exitApp(object obj)
+        {
+            Application.Current.Shutdown();
+        }
+
+        //basic view model base
+        internal void RaisePropertyChanged(string prop)
+        {
+            if(PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs(prop)); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
